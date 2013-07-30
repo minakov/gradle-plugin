0982327a018f33d5ec1b71f7a5ec7acf4f1635ad
Andrew Hawker <andrew@appthwack.com>
2013-07-30 14:24:12 -0700

    Initial commit.

--- src/main/groovy/com/appthwack/AppThwackExtension.groovy
+++ src/main/groovy/com/appthwack/AppThwackExtension.groovy
@@ -17,5 +17,86 @@
 package com.appthwack
 
 class AppThwackExtension {
-    // TODO: figure out the parameters.
+
+    public static final String JUNIT_TYPE = "junit"
+    public static final String CALABASH_TYPE = "calabash"
+    public static final String APP_EXPLORER_TYPE = "appexplorer"
+
+    /**
+     * [Internal] Set the domain of the API endpoint. Used for testing.
+     * Default: 'https://appthwack.com'
+     */
+    String domain = "https://appthwack.com"
+
+    /**
+     * [Required] API Key for AppThwack user account.
+     * See: https://appthwack.com/user/profile for more details.
+     */
+    String apiKey
+
+    /**
+     * [Required] Test type. Supports: calabash, junit, appexplorer
+     * Default: junit
+     */
+    String type = JUNIT_TYPE
+
+    /**
+     * [Required] Name of AppThwack project which contains this application.
+     */
+    String project
+
+    /**
+     * [Optional] Name of test run to be displayed on AppThwack.
+     * Default: <Name of APK (Gradle)>
+     */
+    String name
+
+    /**
+     * [Optional] Name of custom device pool to use.
+     * Default: Top 10
+     */
+    String devicePool = "Top 10"
+
+    /**
+     * [Optional] Fully qualified path to features.zip which contains Calabash test content.
+     * Note: Set by call to calabash()
+     */
+    String calabashContent
+
+    /**
+     * [Optional] Optional execution parameters for the AppThwack AppExplorer.
+     * Note: Set by call to explorer()
+     */
+    public final HashMap<String, String> explorerOptions = new HashMap<String, String>();
+
+    /**
+     * Select Calabash content to use.
+     * @param path path to features.zip
+     */
+    void calabash(String path) {
+        type = CALABASH_TYPE
+        calabashContent = path
+    }
+
+    /**
+     * Default basic AppThwack AppExplorer.
+     * @param username custom username for login credentials
+     * @param password customer password for login credentials
+     * @param launchData URI passed to the app
+     * @param eventCount number of monkey exerciser events
+     * @param monkeySeed random seed used by monkey exerciser
+     */
+    void explorer(String username="",
+                  String password="",
+                  String launchData="",
+                  int eventCount=0,
+                  int monkeySeed=0) {
+
+        type = APP_EXPLORER_TYPE
+        explorerOptions.put("username", username)
+        explorerOptions.put("password", password)
+        explorerOptions.put("launchdata", launchData)
+        explorerOptions.put("eventcount", Integer.toString(eventCount))
+        explorerOptions.put("monkeyseed", Integer.toString(monkeySeed))
+    }
 }
--- src/main/groovy/com/appthwack/AppThwackServer.java
+++ src/main/groovy/com/appthwack/AppThwackServer.java
@@ -16,17 +16,22 @@
 
 package com.appthwack;
 
+import java.io.File;
+import java.util.HashMap;
+
 import com.android.annotations.NonNull;
 import com.android.annotations.Nullable;
 import com.android.builder.testing.api.TestServer;
 import com.android.utils.ILogger;
+import com.appthwack.AppThwackPlugin;
+import com.appthwack.appthwack.*;
 
-import java.io.File;
 
 public class AppThwackServer extends TestServer {
 
     private final AppThwackExtension extension;
     private final ILogger logger;
+    private AppThwackApi api;
 
     AppThwackServer(@NonNull AppThwackExtension extension,
                     @NonNull ILogger logger) {
@@ -34,23 +39,226 @@ public class AppThwackServer extends TestServer {
         this.logger = logger;
     }
 
+    /**
+     * Name of the gradle plugin.
+     * @return appthwack
+     */
     @Override
     public String getName() {
-        return "appthwack";
+        return AppThwackPlugin.PLUGIN_NAME;
     }
 
+    /**
+     * Upload and test the newly built apk.
+     * @param variantName variant of the latest build. Ex: 'debug'
+     * @param testApk File object to the newly built APK which contains tests
+     * @param testedApk File object to the newly built application APK
+     */
     @Override
     public void uploadApks(@NonNull String variantName, @NonNull File testApk, @Nullable File testedApk) {
-        System.out.println(String.format(
-                "APPTHWACK: Variant(%s), Uploading APKs\n\t%s\n\t%s",
-                variantName,
-                testApk.getAbsolutePath(),
-                testedApk != null ? testedApk.getAbsolutePath() : "<none>"));
+        if (testedApk == null) {
+            logger.warning("[AppThwack] Application APK required.");
+            return;
+        }
+
+        //Configure client to use user defined API Key.
+        String apiKey = extension.getApiKey();
+        String domain = extension.getDomain();
+        api = new AppThwackApi(apiKey, domain);
+        System.out.println(String.format("[AppThwack] Using API Key %s.", apiKey));
+
+        String projectName = extension.getProject();
+        System.out.println(String.format("[AppThwack] Using Project '%s'.", projectName));
+
+        //Attempt to get AppThwack project with user defined name.
+        AppThwackProject project = api.getProject(projectName);
+        if (project == null) {
+            logger.warning("[AppThwack] Project '%s' not found.", projectName);
+            return;
+        }
+
+        String devicePoolName = extension.getDevicePool();
+        System.out.println(String.format("[AppThwack] Using DevicePool '%s'.", devicePoolName));
+
+        //Attempt to get AppThwack device pool with user defined name.
+        AppThwackDevicePool devicePool = project.getDevicePool(devicePoolName);
+        if (devicePool == null) {
+            logger.warning("[AppThwack] Device Pool '%s' not found.", devicePoolName);
+            return;
+        }
+
+        System.out.println(String.format("[AppThwack] Uploading apk '%s'.", testedApk.getName()));
+
+        //Upload APK.
+        AppThwackFile app = uploadFile(testedApk);
+        if (app == null) {
+            logger.warning("[AppThwack] Failed to upload apk '%s'.", testedApk.getName());
+            return;
+        }
+
+        //Upload test content.
+        AppThwackFile tests = uploadTestContent(testApk);
+
+        String type = extension.getType();
+        if (tests == null && !type.equalsIgnoreCase(AppThwackExtension.APP_EXPLORER_TYPE)) {
+            logger.warning("[AppThwack] Unable to schedule run, failed to upload required test content.");
+            return;
+        }
+
+        //Test name default is <Apk Name (Gradle)>
+        String name = extension.getName();
+        if (name == null || name.isEmpty()) {
+            name = String.format("%s (Gradle)", testedApk.getName());
+        }
+        System.out.println(String.format("[AppThwack] Scheduling '%s' run '%s'.", type, name));
+
+        //Schedule our test run.
+        AppThwackRun run = scheduleTestRun(project, devicePool, type, name, app, tests, extension.explorerOptions);
+        if (run == null) {
+            logger.warning("[AppThwack] Failed to schedule test run '%s'.", name);
+            return;
+        }
+
+        System.out.println(String.format("[AppThwack] Congrats! See your test results at %s/%s.", domain, run.toString()));
     }
 
+    /**
+     * Verify the AppThwack extension is properly configured.
+     * @return true if configuration is valid, false otherwise.
+     */
     @Override
     public boolean isConfigured() {
-        // TODO: detect authentication is actually configured and return false if not.
+
+        //[Required]: AppThwack Api Key.
+        String apiKey = extension.getApiKey();
+        if (apiKey == null || apiKey.isEmpty()) {
+            logger.warning("AppThwack apiKey is required. See https://appthwack.com/user/profile for details.");
+            return false;
+        }
+
+        //[Required]: AppThwack project name.
+        String project = extension.getProject();
+        if (project == null || project.isEmpty()) {
+            logger.warning("[AppThwack] project name is required.");
+            return false;
+        }
+
+        //[Required]: AppThwack test runner type. Defaults to junit.
+        String type = extension.getType();
+        if (type == null || type.isEmpty()) {
+            logger.warning("[AppThwack] type is required. Expects: junit, calabash, appexplorer.");
+            return false;
+        }
+        if (!(type.equalsIgnoreCase(AppThwackExtension.JUNIT_TYPE)
+                || type.equalsIgnoreCase(AppThwackExtension.CALABASH_TYPE)
+                || type.equalsIgnoreCase(AppThwackExtension.APP_EXPLORER_TYPE))) {
+            logger.warning("[AppThwack] Type is invalid. Expects: junit, calabash, appexplorer.");
+            return false;
+        }
         return true;
+    }
+
+    /**
+     * Upload JUnit/Robotium test apk or Calabash scripts.
+     * @param testApk test apk generated by the build
+     * @return object which represents a remote file on AppThwack
+     */
+    private AppThwackFile uploadTestContent(File testApk) {
+
+        String type = extension.getType();
+
+        //Upload JUnit/Robotium test content (.apk).
+        if (type.equalsIgnoreCase(AppThwackExtension.JUNIT_TYPE)) {
+            if (testApk == null) {
+                logger.warning("[AppThwack] No test apk provided. Unable to run JUnit/Robotium tests.");
+                return null;
+            }
+            System.out.println(String.format("[AppThwack] Uploading test apk '%s'.", testApk.getName()));
+
+            AppThwackFile testApkFile = uploadFile(testApk);
+            if (testApkFile == null) {
+                logger.warning("[AppThwack] Failed to upload test apk '%s'.", testApk.getName());
+                return null;
+            }
+            return testApkFile;
+        }
+
+        //Upload Calabash test content (.zip).
+        if (type.equalsIgnoreCase(AppThwackExtension.CALABASH_TYPE)) {
+            String calabashPath = extension.getCalabashContent();
+            if (calabashPath == null || calabashPath.isEmpty()) {
+                logger.warning("[AppThwack] No content provided. Unable to run Calabash tests.");
+                return null;
+            }
+            File content = new File(calabashPath);
+            if (!content.exists()) {
+                logger.warning("[AppThwack] Calabash content not found at '%s'.", content.getAbsolutePath());
+                return null;
+            }
+            if (!calabashPath.endsWith(".zip")) {
+                logger.warning("[AppThwack] Calabash content must be of type .zip.");
+                return null;
+            }
+            System.out.println(String.format("[AppThwack] Uploading Calabash test content '%s'.", content.getName()));
+
+            AppThwackFile calabashTests = uploadFile(content);
+            if (calabashTests == null) {
+                logger.warning("[AppThwack] Failed to upload Calabash content '%s'.", content.getAbsolutePath());
+                return null;
+            }
+            return calabashTests;
+        }
+        return null;
+    }
+
+    /**
+     * Schedules a test run on AppThwack.
+     * @param project user project which will contain the run
+     * @param pool device pool to run tests on
+     * @param type type of tests to run
+     * @param name name of test run
+     * @param app object returned from uploading user apk
+     * @param tests object returned from uploading user test content
+     * @param options map of optional parameters to configure the AppExplorer
+     * @return object which represents a remote run on AppThwack
+     */
+    private AppThwackRun scheduleTestRun(AppThwackProject project,
+                                        AppThwackDevicePool pool,
+                                        String type,
+                                        String name,
+                                        AppThwackFile app,
+                                        AppThwackFile tests,
+                                        HashMap<String, String> options) {
+
+        try {
+            if (type.equalsIgnoreCase(AppThwackExtension.JUNIT_TYPE)) {
+                return project.scheduleJUnitRun(app, tests, name, pool);
+            }
+            else if (type.equalsIgnoreCase(AppThwackExtension.CALABASH_TYPE)) {
+                return project.scheduleCalabashRun(app, tests, name, pool);
+            }
+            else {
+                return project.scheduleAppExplorerRun(app, name, pool, options);
+            }
+        }
+        catch(AppThwackException e) {
+            logger.error(e, "[AppThwack] Failed to schedule run '%s' of type '%s'.", name, type);
+            return null;
+        }
+    }
+
+    /**
+     * Uploads newly built apk to AppThwack or returns null on error.
+     * @param apk File object of the apk to upload.
+     * @return Object representing a remote file stored on AppThwack
+     */
+    private AppThwackFile uploadFile(File apk) {
+        try {
+            return api.uploadFile(apk);
+        }
+        catch(AppThwackException e) {
+            logger.error(e, "[AppThwack] Failed to upload file '%s'.", apk.getName());
+            return null;
+        }
     }
 }
